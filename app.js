810ff74b98f6924df9265594b2d8b77a4aaab303
KamiyaNya <dikriss16@gmail.com>
2023-12-08 16:03:36 +0300

    Настройка куки и cors

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
-require('dotenv').config();
 const router = require('./routes/routes');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
+require('dotenv').config();
 const app = express();
 const httpServer = createServer(app);
 const PORT = process.env.PORT;
-
 
 const io = new Server(httpServer, {
 	/* options */
@@ -16,11 +17,11 @@ const io = new Server(httpServer, {
 io.on('connection', (socket) => {
 	// ...
 });
-app.use(require('cors')());
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(router);
-
 
 httpServer.listen(PORT, () => {
 	console.log(`server started - http://localhost:${PORT}`);
