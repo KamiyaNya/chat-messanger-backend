33051cbb2a238d03da77ba4bca43081d17da23b9
KamiyaNya <dikriss16@gmail.com>
2023-12-14 21:15:33 +0300

    Проверки

--- socket/socket.js
+++ socket/socket.js
@@ -3,23 +3,26 @@ const prismaInstance = require('../prisma/prismaInstance');
 module.exports = function (io) {
 	io.on('connection', (socket) => {
 		console.log(socket.rooms);
-
 		socket.on('chat-message', async (body) => {
-			console.log(body);
 			const { message, room, userId } = body;
-			await prismaInstance.personalMessages.create({
+			const messageFromBd = await prismaInstance.personalMessages.create({
 				data: {
 					message: message,
 					roomUUID: room,
 					userId: userId,
 				},
 			});
-			// socket.emit('send-message-to-client', body);
-			socket.to(room).emit('send-message-to-client', body);
+			socket.to(room).emit('send-message-to-client', messageFromBd);
+			console.log(socket.rooms);
 		});
 		socket.on('join-room', (body) => {
 			const { room } = body;
 			socket.join(room);
+			console.log(socket.rooms);
 		});
+	});
+
+	io.on('disconnect', (socket) => {
+		console.log(socket);
 	});
 };
