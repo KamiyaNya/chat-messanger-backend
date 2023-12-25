61881c8d79c2f67891a7ab8e3b96ae253612de6e
KamiyaNya <dikriss16@gmail.com>
2023-12-25 20:55:06 +0300

    добавлен маршрут для уведомлений

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const auth = require('./auth.route.js');
 const chat = require('./chat.route.js');
 const users = require('./users.route.js');
+const notifications = require('./notification.route.js');
 
 router.use(auth);
 router.use(chat);
 router.use(users);
+router.use(notifications);
 
 module.exports = router;
